e4205fc8a4dcedc7a10f43efaac8e6db61bffda1
kfj6bnwwyx-alt <kfj6bnwwyx@privaterelay.appleid.com>
2026-04-10 20:46:39 -0400

    Initial Commit

---
+++ house connect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  house connect
+//
+//  Created by brent brooks on 4/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ house connect/house_connectApp.swift
@@ -0,0 +1,17 @@
+//
+//  house_connectApp.swift
+//  house connect
+//
+//  Created by brent brooks on 4/10/26.
+//
+
+import SwiftUI
+
+@main
+struct house_connectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
